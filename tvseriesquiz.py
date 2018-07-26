b5a49b90b34721b0404c8fc71bc1ab49335838e4
Chinmay Khamkar <khamkarchinmay4@gmail.com>
2018-07-26 22:27:59 +0530

    first commit

---
+++ tvseriesquiz.py
@@ -0,0 +1,271 @@
+def breaking_bad():
+    score = 0
+
+    print("what color is marie obsessed with\n")
+    print("options\n1.pink\t2.purple\t3.red\t4.blue\n")
+    ans = int(input("enter yor answer"))
+
+    if ans == 2:
+
+        score = score +4
+        print('right ans!!!!!')
+        print("your score is ",score)
+
+    else:
+        score=score-1
+        print('opps!!! the right ans was 2\n')
+        print("your score is",score)
+    print("how old is walt in the last episode\n")
+    print("options\n1.50\tt2.51\t3.52\t4.53\n")
+    ans = int(input("enter yor answer"))
+    if ans == 3:
+        score=score+4
+        print('right ans!!!!!')
+        print("your score is ", score)
+
+    else:
+        score = score - 1
+        print('opps!!! the right ans was 3\n')
+        print("your score is", score)
+    print("who hooks jesse up with tuco\n")
+    print("options\n1.skinny pete\t2.badger\t3.krazy8\t4.combo\n")
+    ans = int(input("enter yor answer"))
+
+    if ans == 1:
+
+        score = score + 4
+        print('right ans!!!!!')
+        print("your score is ", score)
+
+    else:
+        score = score - 1
+        print('opps!!! the right ans was 1\n')
+        print("your score is", score)
+    print("whats the name of the plant used to poison brock\n")
+    print("options\n1.water lily\t2.lily of the nile\t3.paradise lily\t4.lily of the valley\n")
+    ans = int(input("enter yor answer"))
+
+    if ans == 4:
+
+        score = score + 4
+        print('right ans!!!!!')
+        print("your score is ", score)
+
+    else:
+        score = score - 1
+        print('opps!!! the right ans was 4\n')
+        print("your score is", score)
+    print("what country does guss claim to be from\n")
+    print("options\n1.argentina\t2.columbia\t3.mexico\t4.chile\n")
+    ans = int(input("enter yor answer"))
+
+    if ans == 4:
+
+        score = score + 4
+        print('right ans!!!!!')
+        print("your score is ", score)
+
+    else:
+        score = score - 1
+        print('opps!!! the right ans was 4\n')
+        print("your score is", score)
+    print("which episode is not a season finale\n")
+    print("options\n1.half measures\t2.full measures\t3.ABQ\t4.face off\n")
+    ans = int(input("enter yor answer"))
+
+    if ans == 1:
+
+        score = score + 4
+        print('right ans!!!!!')
+        print("your score is ", score)
+
+    else:
+        score = score - 1
+        print('opps!!! the right ans was 1\n')
+        print("your score is", score)
+    print("who is first person jesse calls after jane's death\n")
+    print("options\n1.bagder\t2.walt\t3.mike\t4.saul\n")
+    ans = int(input("enter yor answer"))
+
+    if ans == 2:
+
+        score = score + 4
+        print('right ans!!!!!')
+        print("your score is ", score)
+
+    else:
+        score = score - 1
+        print('opps!!! the right ans was 2\n')
+        print("your score is", score)
+    print("whats the purity level of walt's meth\n")
+    print("options\n1.99.9%\t2.100%\t3.99.1%\t4.96%\n")
+    ans = int(input("enter yor answer"))
+
+    if ans == 3:
+
+        score = score + 4
+        print('right ans!!!!!')
+        print("your score is ", score)
+
+    else:
+        score = score - 1
+        print('opps!!! the right ans was 3\n')
+        print("your score is", score)
+    print("whats mike's granddaughter's name\n")
+    print("options\n1.bailey\t2.riley\t3.hailee\t4.kaylee\n")
+    ans = int(input("enter yor answer"))
+
+    if ans == 4:
+
+        score = score + 4
+        print('right ans!!!!!')
+        print("your score is ", score)
+
+    else:
+        score = score - 1
+        print('opps!!! the right ans was 4\n')
+        print("your score is", score)
+def death_note ():
+    score = 0
+
+    print("what is a shinigami\n")
+    print("options\n1.god of death\t2.a soul in peace\t3.an alien\t4.other world monster\n")
+    ans = int(input("enter yor answer"))
+
+    if ans == 1:
+
+        score = score + 4
+        print('right ans!!!!!')
+        print("your score is ", score)
+
+    else:
+        score = score - 1
+        print('opps!!! the right ans was 1\n')
+        print("your score is", score)
+    print("who did the original death note belong to\n")
+    print("options\n1.it had no owner\t2.ryuk\t3.it fell from heaven\t4.a wizard\n")
+    ans = int(input("enter yor answer"))
+    if ans == 2:
+        score = score + 4
+        print('right ans!!!!!')
+        print("your score is ", score)
+
+    else:
+        score = score - 1
+        print('opps!!! the right ans was 2\n')
+        print("your score is", score)
+    print("what do u need when you add someone to the death note\n")
+    print("options\n1.none\t2.name only\t3.name and face\t4.only face\n")
+    ans = int(input("enter yor answer"))
+
+    if ans == 3:
+
+        score = score + 4
+        print('right ans!!!!!')
+        print("your score is ", score)
+
+    else:
+        score = score - 1
+        print('opps!!! the right ans was 3\n')
+        print("your score is", score)
+    print("what do u need to do to see the shinigami owner of a death note\n")
+    print("options\n1.write sinigami's name 3 times in the book\t2.offer sginigami an apple\t3.kill your first victim\t4.simply touch the death note\n")
+    ans = int(input("enter yor answer"))
+
+    if ans == 4:
+
+        score = score + 4
+        print('right ans!!!!!')
+        print("your score is ", score)
+
+    else:
+        score = score - 1
+        print('opps!!! the right ans was 4\n')
+        print("your score is", score)
+    print("what must be sacrificed to obtain the power of shinigami eyes\n")
+    print("options\n1.ability to hear\t2.your natural sight\t3.the life of a loved one\t4.half of your life span\n")
+    ans = int(input("enter yor answer"))
+
+    if ans == 4:
+
+        score = score + 4
+        print('right ans!!!!!')
+        print("your score is ", score)
+
+    else:
+        score = score - 1
+        print('opps!!! the right ans was 4\n')
+        print("your score is", score)
+    print("what do u gain from using the power of shinigami eyes\n")
+    print("options\n1.change the past\t2.see the future\t3.read people's thought\t4.name and life expectancies of others\n")
+    ans = int(input("enter yor answer"))
+
+    if ans == 4:
+
+        score = score + 4
+        print('right ans!!!!!')
+        print("your score is ", score)
+
+    else:
+        score = score - 1
+        print('opps!!! the right ans was 4\n')
+        print("your score is", score)
+    print("what is the only thing that can kill a shinigami\n")
+    print("options\n1.they do not die\t2.writing their name in the book\t3.with a spell\t4.saving humans life\n")
+    ans = int(input("enter yor answer"))
+
+    if ans == 4:
+
+        score = score + 4
+        print('right ans!!!!!')
+        print("your score is ", score)
+
+    else:
+        score = score - 1
+        print('opps!!! the right ans was 4\n')
+        print("your score is", score)
+    print("what happens if u tear a page form a death note\n")
+    print("options\n1.it catches fire\t2.you die\t3.works the same \t4.it losses its power\n")
+    ans = int(input("enter yor answer"))
+
+    if ans == 3:
+
+        score = score + 4
+        print('right ans!!!!!')
+        print("your score is ", score)
+
+    else:
+        score = score - 1
+        print('opps!!! the right ans was 3\n')
+        print("your score is", score)
+    print("how many pages ARE IN A DEATH NOTE\n")
+    print("options\n1.100\t2.666\t3.1000\t4.infinite\n")
+    ans = int(input("enter yor answer"))
+
+    if ans == 4:
+
+        score = score + 4
+        print('right ans!!!!!')
+        print("your score is ", score)
+
+    else:
+        score = score - 1
+        print('opps!!! the right ans was 4\n')
+        print("your score is", score)
+
+
+
+print ("***********welcome to the quiz game************\n")
+print ('u will be asked questions based on your favorite tv shows\n')
+print ('rules for the quiz\n1)there would be 10 questions in each set\n2)+4 for right answer and -1 for wrong ans')
+print ('select any tv show of your choice')
+print('1)breaking bad\n2)F.R.I.E.N.D.S\n3)death note')
+c =  int (input("your choice"))
+if c == 1:
+    breaking_bad()
+elif c == 2:
+    friends()
+elif c==3:
+    death_note()
+else:
+    print("invalid choice")
